6374c58161cfd3eb14817b7f1c94a11d09fb55ae
Clara Grazian <z3528244@matht476.maths.unsw.edu.au>
2020-06-11 15:46:05 +1000

    Adaptive Tolerance

--- R/abc.R
+++ R/abc.R
@@ -14,18 +14,17 @@
 #' @param b3 Maximum of the uniform prior for K3. Default to 0.1 .
 #' @param a4 Minimum of the uniform prior for K4. Default to 0 .
 #' @param b4 Maximum of the uniform prior for K4. Default to 0.2 .
-#' @param tol Tolerance level of the ABC function. This is the required proportion
-#' of points accepted nearest to the target values. Default at 0.05.
 #' @param method Parameter of the ABC algorithm: a character string indicating
 #' the type of ABC algorithm to be applied. Possible values are "rejection",
 #' "loclinear", "neuealnet", and "ridge". Default "regression".
+#' @param PLOT If plots have to be produced. Default at FALSE.
 #' @return ABCres_single an object of class abc for the single-tissue compartment model.
 #' @return ABCres_two an object of class abc for the two-tissue compartment model.
 #' @keywords PETabc
 #' @export
-PETabc <- function(y,t,N=100000, inputfunction.=inputfunction, type=2,
+PETabc <- function(y,tspan,N=100000, inputfunction.=inputfunction, type=2,
                    a1=0,b1=0.2,a2=0.3,b2=0.5,a3=0,b3=0.1,a4=0,b4=0.2,
-                   tol=0.05,method="rejection")
+                   method="rejection", PLOT=F)
 {
   # y: vector of measured radioactivity concentrations in a voxel for each time-point. This can be in terms of observations or directly in terms of voxel time activity curve.
   # t: vector of time points of observations
@@ -77,15 +76,17 @@ PETabc <- function(y,t,N=100000, inputfunction.=inputfunction, type=2,
     return(list(c(dydt)))
   }
 
-  Sobs=smooth.spline(time_vec,y, cv=T)$y
+  Sobs=smooth.spline(tspan,y, cv=T)$y
   #Sobs= ksmooth(c(1:61), y2, bandwidth=2)$y
   #Smat1=Smat2=matrix(NA, ncol=length(Sobs), nrow=N)
-  tspan=time_vec #time interval and sampling
+  #time interval and sampling
 
-  Smat1 <- matrix(NA, nrow=N,ncol=length(time_vec))
-  Smat2 <- matrix(NA, nrow=N,ncol=length(time_vec))
+  Smat1 <- matrix(NA, nrow=N,ncol=length(tspan))
+  Smat2 <- matrix(NA, nrow=N,ncol=length(tspan))
+  error1=error2=matrix(NA, ncol=1, nrow=N)
+
   names.Smat <- c()
-    for(j in 1:length(time_vec)){
+    for(j in 1:length(tspan)){
       names.Smat[j] <- paste("y",j,sep="")
   }
   colnames(Smat1) <- names.Smat
@@ -102,9 +103,6 @@ PETabc <- function(y,t,N=100000, inputfunction.=inputfunction, type=2,
       K3=runif(1, a3, b3)
       K4=runif(1, a4, b4)
 
-      parMat1[i,] <- c(K1,K2)
-      parMat2[i,] <- c(K1,K2,K3,K4)
-
       parms1=c(K1, K2, type)
       parms2=c(K1, K2, K3, K4, type)
     # Ct0=0 #initial condition : cncentration=0 at t=0
@@ -116,11 +114,13 @@ PETabc <- function(y,t,N=100000, inputfunction.=inputfunction, type=2,
     data1=t(out[,2])
     Smat1[i,]=as.vector(data1)
     #Smat1=smooth.spline(time_vec, data1, cv=T)$y
+    error1[i,]=sum((y-Smat1[i,])^2)
 
     out=ode(c(0,0), tspan, twotissue, parms2, method="ode45")
     data2=t(out[,2])+t(out[,3])
     #Smat2=as.vector(data2)
     Smat2[i,]=smooth.spline(time_vec, data2, cv=T)$y
+    error2[i,]=sum((y-Smat2[i,])^2)
 
 #    write(c(K1, K2, K3, K4), file="parMat.out", ncol=4, append=T)
 #    write(t(Smat1), file="SMat1.out", ncol=length(time_vec), append=T)
@@ -130,17 +130,115 @@ PETabc <- function(y,t,N=100000, inputfunction.=inputfunction, type=2,
 #    write(t(smooth.spline(time_vec, Smat2, cv=T)$y), ncol=length(time_vec),
 #          file="Smat2S.out", append=T)
 
+    parMat1[i,] <- c(K1,K2)
+    parMat2[i,] <- c(K1,K2,K3,K4)
   }
 
-  out1=abc(target=Sobs, param=parMat1, sumstat=Smat1, tol=tol,
-           method=method)
-  out2=abc(target=Sobs, param=parMat2, sumstat=Smat2, tol=tol,
-           method=method)
+  h1=apply(error1, 2, quantile, probs=0.05)
+  h2=apply(error2, 2, quantile, probs=0.05)
+
+  parM1=apply(parMat1[(error1[,1]<h1[1])==1,], 2, mean)
+  parM2=apply(parMat2[(error2[,1]<h2[1])==1,], 2, mean)
+
+  out1=abc(target=Sobs, param=parMat1, sumstat=Smat1, tol=h1,
+             method=method)
+  out2=abc(target=Sobs, param=parMat2, sumstat=Smat2, tol=h2,
+              method=method)
+
+  hgrid=seq(0.05, 0.4, length=10)
+  RMSE1=RMSE2=matrix(NA, nrow=1, ncol=length(hgrid))
+  mprob1=mprob2=matrix(NA, ncol=length(hgrid), nrow=1)
+
+  for (j in c(1:length(hgrid))) {
+    ind=error1[,1]<hgrid[j]
+    RMSE1[1, j]=sqrt(mean((apply(parMat1[ind==1,], 2, mean)-
+                             parM1)^2))
+    mprob1[1, j]=sum(ind)
+  }
+
+  for (j in c(1:length(hgrid))) {
+    ind=error2[,1]<hgrid[j]
+    RMSE2[1, j]=sqrt(mean((apply(parMat2[ind==1,], 2,
+                                 mean)-parM2)^2))
+    mprob2[1, j]=sum(ind)
+  }
+  prob=mprob1/(mprob1+mprob2)
+
+  if(PLOT==T){
+    pdf("modelprob_TAC.pdf")
+      par(mfrow=c(1,1))
+      plot(y, type="p", lwd=3, xlab="time", ylab="Observations",
+           ylim=c(0, 0.3), cex.lab=1, cex.axis=0.8)
+      parms1=c(parM1,  type)
+       #time interval and sampling
+      out=ode(0, tspan, singtissue, parms1, method="ode45")
+      data1sim=as.vector(t(out[,2]))
+      lines(data1sim, lty=1, lwd=3, col=1)
+
+      parms2=c(parM2,  type)
+      out=ode(c(0,0), tspan, twotissue, parms2, method="ode45")
+      data2sim=as.vector(t(out[,2])+t(out[,3]))
+      lines(data2sim, lty=2, lwd=3, col=1)
+
+      legend("topright",c("single","two"),lty=c(1,2),lwd=2)
+    dev.off()
+
+#    par(mfrow=c(1,1))
+#    plot(hgrid, prob[1,], ylim=c(0,1),type="l")
+#    lines(hgrid, RMSE1[1,], col=2)
+#    lines(hgrid, RMSE2[1,], col=3)
+
+    pdf("modelprobWW_M1.pdf")
+        par(mfrow=c(1,1))
+        plot(hgrid, prob[1,], type="l", lwd=3, col=1, lty=1,
+             xlab="tolerance", ylab="model prob", ylim=c(0,1),
+             cex.lab=1.6,cex.axis=0.8)
+        indgrid1=which(RMSE1[1,]==min(RMSE1[1,], na.rm=T))
+        #half way betwwen the two minimums
+        indgrid2=which(RMSE2[1,]==min(RMSE2[1,], na.rm=T))
+        hh=(hgrid[indgrid1]+ hgrid[indgrid2])/2
+        abline(v=hh)
+    dev.off()
+
+    pdf("modelprobWW_RMSE.pdf") #WW=1
+      par(mfrow=c(1,1))
+      plot(hgrid, RMSE1[1,], type="l", lty=1, lwd=3, col=1, xlab="tolerance",
+           ylab="RMSE", cex.lab=1.6, cex.axis=0.8)
+      lines(hgrid, RMSE2[1,], lty=2, lwd=3, col=1)
+      indgrid1=which(RMSE1[1,]==min(RMSE1[1,], na.rm=T))
+      #half way betwwen the two minimums
+      indgrid2=which(RMSE2[1, ]==min(RMSE2[1, ], na.rm=T))
+      hh=(hgrid[indgrid1]+ hgrid[indgrid2])/2
+      abline(v=hh)
+    dev.off()
+
+    }
+
+#  out1=abc(target=Sobs, param=parMat1, sumstat=Smat1, tol=tol,
+#           method=method)
+#  out2=abc(target=Sobs, param=parMat2, sumstat=Smat2, tol=tol,
+#           method=method)
   #  adj=out$adj.values
   #  unadj=out$unadj.values
 
-  return(list(ABCres_single=out1,ABCres_two=out2))
+  write(t(parMat1), file="parMat1.out", ncol=4)
+  write(t(parMat2), file="parMat2.out", ncol=4)
+  write(t(Smat1), file="SMat1.out", ncol=60)
+  write(t(Smat2), file="SMat2.out", ncol=60)
+  write(t(error1), file="error1.out", ncol=3)
+  write(t(error2), file="error2.out", ncol=3)
 
+  if(method == "rejection"){
+    return(list(ABCres_single=list(ABCout=parMat1,Smat=Smat1,error=error1,tol=h1,
+                                   ABCout_unadj=out1$unadj.values,method=method),
+                ABCres_two=list(ABCout=parMat1,Smat=Smat1,error=error1,tol=h2,
+                                ABCout_unadj=out2$unadj.values,method=method) ) )
+  } else {
+    return(list(ABCres_single=list(ABCout=parMat1,Smat=Smat1,error=error1,tol=h1,
+                                   ABCout_adj=out1$adj.values,method=method),
+                ABCres_two=list(ABCout=parMat1,Smat=Smat1,error=error1,tol=h2,
+                                  ABCout_adj=out2$adj.values,method=method) ) )
+  }
 }
 
 #
